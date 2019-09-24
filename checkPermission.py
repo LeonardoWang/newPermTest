a009d24942e13eed7c8bb4b2d7b34240fcc93eb7
leonardo <806218468@qq.com>
2019-09-24 22:47:58 +0800

    test

--- checkPermission.py
+++ checkPermission.py
@@ -69,7 +69,12 @@ def get_dex_file(file_path):
                         library_list.append(lib)
                     break
                     new_class_name = tmp_class_name.replace('/','.')
-                    
+            
+            if 'android.permission.RECEIVE_BOOT_COMPLETED' in pscout_perms:
+                print('!!!pscout')
+            if 'android.permission.RECEIVE_BOOT_COMPLETED' in axplorer_perms:
+                print('!!!axplorer')
+
             for perm in pscout_perms:
             	if perm in total_permission_list and perm not in usage_perm_list:
             		usage_perm_list.append(perm)
